b4cfd3b45292b2dde1a93efa9813ae0f6d94b0b2
Deniz <auo@tuta.io>
2025-06-01 20:15:36 +0200

    major style changes

--- data.js
+++ data.js
@@ -28,6 +28,34 @@ export const herbsData = [
         alt: "Photo of thyme plant."
     },
     {
+        benefits: ["medicinal", "anti-inflammatory", "anti-oxidant", "digestive", "immune-booster"],
+        name: "Ginger",
+        isCulinary: true,
+        image: "ginger.jpg",
+        alt: "Photo of ginger root."
+    },
+    {
+        benefits: ["medicinal", "sedative", "calming", "anti-stress", "sedative"],
+        name: "Lavender",
+        isCulinary: true,
+        image: "lavender.jpg",
+        alt: "Photo of lavender  plant."
+    },
+    {
+        benefits: ["medicinal", "sedative", "anti-stress", "digestive", "anti-inflammatory", "skin-soothing"],
+        name: "Chamomile",
+        isCulinary: true,
+        image: "chamomile.jpg",
+        alt: "Photo of chamomile  plant."
+    },
+    {
+        benefits: ["medicinal", "sedative", "anti-stress", "digestive"],
+        name: "Melissa",
+        isCulinary: true,
+        image: "melissa.jpg",
+        alt: "Photo of St. Melissa  plant."
+    },
+    {
         benefits: ["medicinal", "immune-booster"],
         name: "echinacea",
         isCulinary: false,
@@ -35,160 +63,17 @@ export const herbsData = [
         alt: "Photo of echinacea plant."
     },
     {
-        benefits: ["medicinal", "anti-inflammatory", "anti-bacterial"],
+        benefits: ["medicinal", "anti-inflammatory", "anti-bacterial", "anti-stress"],
         name: "St. John's Wort",
         isCulinary: false,
         image: "st-johns-wort.jpg",
         alt: "Photo of St. John's Wort plant."
     },
     {
-        benefits: ["confused", "sad"],
+        benefits: ["medicinal", "anti-inflammatory", "astringent", "skin-soothing"],
+        name: "Witch Hazel",
         isCulinary: false,
-        image: "confused.jpeg",
-        alt: "A cat looking confused"
+        image: "witch-hazel.jpg",
+        alt: "Photo of witch hazel plant."
     },
-    {
-        benefits: ["dominant", "moody"],
-        isCulinary: false,
-        image: "dominant.jpeg",
-        alt: "A cat looking dominant"
-    },
-    {
-        benefits: ["happy", "relaxed"],
-        isCulinary: false,
-        image: "happy.jpeg",
-        alt: "A cat looking happy"
-    },
-    {
-        benefits: ["hungry"],
-        isCulinary: false,
-        image: "hungry.jpeg",
-        alt: "A cat looking hungry"
-    },
-    {
-        benefits: ["hungry"],
-        isCulinary: false,
-        image: "hungry1.jpeg",
-        alt: "A cat looking hungry"
-    },
-    {
-        benefits: ["insomniac"],
-        isCulinary: false,
-        image: "insomnia.jpeg",
-        alt: "A cat looking insomniac"
-    },
-    {
-        benefits: ["insomniac"],
-        isCulinary: false,
-        image: "insomnia1.jpeg",
-        alt: "A cat looking insomniac"
-    },
-    {
-        benefits: ["relaxed"],
-        isCulinary: false,
-        image: "lazy.jpeg",
-        alt: "A cat looking lazy"
-    },
-    {
-        benefits: ["scared"],
-        isCulinary: false,
-        image: "nervous.jpeg",
-        alt: "A cat looking nervous"
-    },
-    {
-        benefits: ["sad"],
-        isCulinary: false,
-        image: "sad.jpeg",
-        alt: "A cat looking sad"
-    },
-    {
-        benefits: ["sad", "moody"],
-        isCulinary: false,
-        image: "sad1.jpeg",
-        alt: "A cat looking sad"
-    },
-    {
-        benefits: ["moody"],
-        isCulinary: true,
-        image: "angry.gif",
-        alt: "A cat looking moody"
-    },
-    {
-        benefits: ["moody"],
-        isCulinary: true,
-        image: "angry2.gif",
-        alt: "A cat looking angry"
-    },
-    {
-        benefits: ["confused"],
-        isCulinary: true,
-        image: "confused2.gif",
-        alt: "A cat looking confused"
-    },
-    {
-        benefits: ["dominant"],
-        isCulinary: true,
-        image: "dominant.gif",
-        alt: "A cat looking dominant"
-    },
-    {
-        benefits: ["happy"],
-        isCulinary: true,
-        image: "happy.gif",
-        alt: "A cat looking happy"
-    },
-    {
-        benefits: ["hungry", "sad", "confused"],
-        isCulinary: true,
-        image: "confused.gif",
-        alt: "A cat looking hungry"
-    },
-    {
-        benefits: ["hungry"],
-        isCulinary: true,
-        image: "hungry.gif",
-        alt: "A cat looking hungry"
-    },
-    {
-        benefits: ["hungry"],
-        isCulinary: true,
-        image: "hungry2.gif",
-        alt: "A cat looking hungry"
-    },
-    {
-        benefits: ["insomniac", "scared"],
-        isCulinary: true,
-        image: "insomnia2.gif",
-        alt: "A cat looking insomniac"
-    },
-    {
-        benefits: ["relaxed"],
-        isCulinary: true,
-        image: "lazy.gif",
-        alt: "A cat looking relaxed"
-    },
-    {
-        benefits: ["relaxed"],
-        isCulinary: true,
-        image: "relaxed2.gif",
-        alt: "A cat looking relaxed"
-    },
-    {
-        benefits: ["scared", "sad"],
-        isCulinary: true,
-        image: "nervous.gif",
-        alt: "A cat looking nervous"
-    },
-    {
-        benefits: ["scared"],
-        isCulinary: true,
-        image: "nervous2.gif",
-        alt: "A cat looking scared"
-    },
-    {
-        benefits: ["sad"],
-        isCulinary: true,
-        image: "sad.gif",
-        alt: "A cat looking sad"
-    }
-];
+    ];
--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 import { herbsData } from "./data.js";
 
 const benefitSelect = document.getElementById("benefit");
-const getImageBtn = document.getElementById("get-image-btn");
-const gifsOnlyOption = document.getElementById("gifs-only-option");
+const getImageBtn = document.getElementById("see-herbs-btn");
+const culinaryOnlyOption = document.getElementById("culinary-only-option");
 const memeModalInner = document.getElementById("meme-modal-inner");
 const memeModal = document.getElementById("meme-modal");
 const memeModalCloseBtn = document.getElementById("meme-modal-close-btn");
@@ -63,7 +63,7 @@ function renderHerb() {
 
 function getMatchingHerbsArray() {
     const selectedBenefit = benefitSelect.value;
-    const isCulinary = gifsOnlyOption.checked;
+    const isCulinary = culinaryOnlyOption.checked;
 
     const matchingHerbsArray = herbsData.filter(function (herb) {
         if (isCulinary) {
